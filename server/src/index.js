e6f420f9e8071df7ae413c9f3b6f4bcbfd882a23
Lee Hyohwak <crewe1746@naver.com>
2023-07-23 18:41:03 +0900

    Update server dependencies

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('OK');
+});
+
+app.listen(8080, () => {
+  console.log('Listening on port 8080');
+});
