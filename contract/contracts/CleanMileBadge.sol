d8ceb9971223d11fcb15b952c5c3a2f843574378
BEB 09 박상현 <psh03225@naver.com>
2023-07-26 11:40:39 +0900

    update CleanMileDNFT.sol remove onlyowner

--- contract/contracts/CleanMileBadge.sol
+++ contract/contracts/CleanMileBadge.sol
@@ -6,11 +6,11 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "./ICleanMileBadge.sol";
 
-contract CleanMileBadge is ERC1155, ICleanMileBadge ,Ownable {
+contract CleanMileBadge is ERC1155, ICleanMileBadge, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
 
-    uint256[3] badgeScore = [1,5,10];
+    uint256[3] badgeScore = [1, 5, 10];
 
     // 뱃지의 URI를 저장하기 위한 매핑입니다.
     mapping(uint256 => string) private _badgeURIs;
@@ -27,12 +27,22 @@ contract CleanMileBadge is ERC1155, ICleanMileBadge ,Ownable {
     // 각 사용자에 대한 뱃지 스코어를 저장하는 매핑입니다.
     mapping(address => uint256) private _userBadgeScore;
 
-    event TransferBatch(address indexed sender, address[] recipients, uint256 tokenId, uint256 amount);
+    event TransferBatch(
+        address indexed sender,
+        address[] recipients,
+        uint256 tokenId,
+        uint256 amount
+    );
 
     constructor() ERC1155("") {}
 
     // 뱃지를 발급하는 함수입니다.
-    function mintBadge(address account, BadgeType badgeType, uint256 amount, string memory _uri) external onlyOwner {
+    function mintBadge(
+        address account,
+        BadgeType badgeType,
+        uint256 amount,
+        string memory _uri
+    ) external onlyOwner {
         require(amount > 0, "Amount should be greater than 0");
 
         uint256 tokenId = _tokenIdCounter.current();
@@ -48,18 +58,33 @@ contract CleanMileBadge is ERC1155, ICleanMileBadge ,Ownable {
     }
 
     // ERC-1155 표준에 정의된 URI를 가져오기 위한 함수입니다.
-    function uri(uint256 tokenId) public view override(ERC1155) returns (string memory) {
+    function uri(
+        uint256 tokenId
+    ) public view override(ERC1155) returns (string memory) {
         return _badgeURIs[tokenId];
     }
 
     // 사용자의 특정 뱃지 수량을 가져오는 함수입니다.
-    function badgeBalance(address account, uint256 tokenId) public view returns (uint256) {
+    function badgeBalance(
+        address account,
+        uint256 tokenId
+    ) public view returns (uint256) {
         return _badgeBalances[tokenId][account];
     }
 
     // 뱃지 전송을 위한 함수입니다. 한명에 사용자에게 한개씩 전송하는 함수 입니다.
-    function transferBadge(address from, address to, uint256 tokenId, uint256 amount) public {
-        require(from == msg.sender || isApprovedForAll(from, msg.sender) || approvalForToken(msg.sender, tokenId), "Caller is not approved");
+    function transferBadge(
+        address from,
+        address to,
+        uint256 tokenId,
+        uint256 amount
+    ) public {
+        require(
+            from == msg.sender ||
+                isApprovedForAll(from, msg.sender) ||
+                approvalForToken(msg.sender, tokenId),
+            "Caller is not approved"
+        );
 
         uint256 senderBalance = _badgeBalances[tokenId][from];
         require(senderBalance >= amount, "Insufficient balance");
@@ -72,30 +97,44 @@ contract CleanMileBadge is ERC1155, ICleanMileBadge ,Ownable {
         emit TransferSingle(msg.sender, from, to, tokenId, amount);
     }
 
+    // 토큰 주인(권한 부여 받은 사람X)이 한가지 종류의 토큰을 여러 사람에게 전송합니다.
+    function transferBadges(
+        address[] calldata recipients,
+        uint256 tokenId,
+        uint256 amount
+    ) public {
+        require(
+            recipients.length > 0,
+            "Input arrays should have the same length"
+        );
 
-        // 토큰 주인(권한 부여 받은 사람X)이 한가지 종류의 토큰을 여러 사람에게 전송합니다.
-    function transferBadges(address[] calldata recipients, uint256 tokenId, uint256 amount) public {
-        require( recipients.length >0 , "Input arrays should have the same length");
-
-        uint256 currentBadageScore = badgeScore[uint256(_tokenToBadgeType[tokenId])];
+        uint256 currentBadageScore = badgeScore[
+            uint256(_tokenToBadgeType[tokenId])
+        ];
 
         for (uint256 i = 0; i < recipients.length; i++) {
             address to = recipients[i];
 
             require(to != address(0), "Invalid recipient");
             require(amount > 0, "Amount should be greater than 0");
-            require(_badgeBalances[tokenId][msg.sender] >= amount, "Insufficient balance");
+            require(
+                _badgeBalances[tokenId][msg.sender] >= amount,
+                "Insufficient balance"
+            );
 
             _badgeBalances[tokenId][msg.sender] -= amount;
             _badgeBalances[tokenId][to] += amount;
 
             _userBadgeScore[to] += currentBadageScore;
         }
-        emit TransferBatch(msg.sender, recipients,tokenId, amount);
+        emit TransferBatch(msg.sender, recipients, tokenId, amount);
     }
 
     // ERC-1155 표준에 정의된 isApprovedForAll 함수를 오버라이드합니다.
-    function isApprovedForAll(address account, address operator) public view override returns (bool) {
+    function isApprovedForAll(
+        address account,
+        address operator
+    ) public view override returns (bool) {
         // 컨트랙트 소유자에 대해 모든 연산자가 허용됩니다.
         if (msg.sender == owner()) {
             return true;
@@ -105,7 +144,11 @@ contract CleanMileBadge is ERC1155, ICleanMileBadge ,Ownable {
     }
 
     // 사용자가 특정 연산자에게 토큰 전송 권한을 부여하는 함수입니다.
-    function setApprovalForAll(address account, address operator, bool approved) public virtual  {
+    function setApprovalForAll(
+        address account,
+        address operator,
+        bool approved
+    ) public virtual {
         require(operator != msg.sender, "You cannot approve yourself");
 
         // 연산자에 대한 권한을 설정합니다.
@@ -113,16 +156,27 @@ contract CleanMileBadge is ERC1155, ICleanMileBadge ,Ownable {
     }
 
     // 특정 토큰에 대한 권한을 다른 계정에게 부여하는 함수입니다.
-    function setApprovalForToken(address account, uint256 tokenId, bool approved) public virtual onlyOwner {
+    function setApprovalForToken(
+        address account,
+        uint256 tokenId,
+        bool approved
+    ) public virtual onlyOwner {
         _tokenApprovals[tokenId][account] = approved;
         emit ApprovalForToken(account, tokenId, approved);
     }
 
     // 특정 토큰에 대한 권한 부여 이벤트입니다.
-    event ApprovalForToken(address indexed account, uint256 indexed tokenId, bool approved);
+    event ApprovalForToken(
+        address indexed account,
+        uint256 indexed tokenId,
+        bool approved
+    );
 
     // 특정 토큰에 대한 권한 여부를 조회하는 함수입니다.
-    function approvalForToken(address account, uint256 tokenId) public view returns (bool) {
+    function approvalForToken(
+        address account,
+        uint256 tokenId
+    ) public view returns (bool) {
         return _tokenApprovals[tokenId][account];
     }
 
@@ -132,7 +186,7 @@ contract CleanMileBadge is ERC1155, ICleanMileBadge ,Ownable {
     }
 
     // 유저의 뱃지 점수를 조회하는 함수입니다.
-    function userBadgeScore(address _address) public view returns(uint256) {
+    function userBadgeScore(address _address) public view returns (uint256) {
         return _userBadgeScore[_address];
     }
 }
--- contract/contracts/CleanMileDNFT.sol
+++ contract/contracts/CleanMileDNFT.sol
@@ -11,13 +11,13 @@ error NotOwner(uint256 _tokenId, address sender);
 error NonexistentToken(uint256 _tokenId);
 error MaxLevel(uint256 _tokenId);
 
-contract CleanMileDNFT is ERC721, Ownable ,ICleanMileDNFT {
+contract CleanMileDNFT is ERC721, Ownable, ICleanMileDNFT {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
 
     ICleanMileBadge private _badge;
 
-    string[] IpfsUri = [ 
+    string[] IpfsUri = [
         "https://gold-cool-goat-213.mypinata.cloud/ipfs/QmfFbvLH37DebBqmVBm7V8ecfzgjFPnPeHRYiYk1PNoW84/1level.png",
         "https://gold-cool-goat-213.mypinata.cloud/ipfs/QmfFbvLH37DebBqmVBm7V8ecfzgjFPnPeHRYiYk1PNoW84/2level.png",
         "https://gold-cool-goat-213.mypinata.cloud/ipfs/QmfFbvLH37DebBqmVBm7V8ecfzgjFPnPeHRYiYk1PNoW84/3level.png",
@@ -35,7 +35,10 @@ contract CleanMileDNFT is ERC721, Ownable ,ICleanMileDNFT {
     mapping(uint256 => DNFTData) private _dnftData;
     mapping(uint256 => string) private _tokenURIs; // Added _tokenURIs mapping
 
-    constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
+    constructor(
+        string memory name,
+        string memory symbol
+    ) ERC721(name, symbol) {}
 
     function mintDNFT(
         address _to,
@@ -48,18 +51,27 @@ contract CleanMileDNFT is ERC721, Ownable ,ICleanMileDNFT {
         _mint(_to, tokenId);
         _setTokenURI(tokenId, _tokenURI);
 
-        _dnftData[tokenId] = DNFTData({ name: _name ,description: _description, dnftType: DnftType.level_1});
+        _dnftData[tokenId] = DNFTData({
+            name: _name,
+            description: _description,
+            dnftType: DnftType.level_1
+        });
     }
 
-    function updateDescription(uint256 _tokenId, string calldata _description) onlyOwner external {
-        if (ownerOf(_tokenId) != msg.sender) revert NotOwner(_tokenId, msg.sender);
+    function updateDescription(
+        uint256 _tokenId,
+        string calldata _description
+    ) external {
+        if (ownerOf(_tokenId) != msg.sender)
+            revert NotOwner(_tokenId, msg.sender);
         _dnftData[_tokenId].description = _description;
 
         emit UpgradeDNFT(msg.sender, _tokenId);
     }
 
-    function updateName(uint256 _tokenId, string calldata _name) onlyOwner external {
-        if (ownerOf(_tokenId) != msg.sender) revert NotOwner(_tokenId, msg.sender);
+    function updateName(uint256 _tokenId, string calldata _name) external {
+        if (ownerOf(_tokenId) != msg.sender)
+            revert NotOwner(_tokenId, msg.sender);
         _dnftData[_tokenId].name = _name;
 
         emit UpgradeDNFT(msg.sender, _tokenId);
@@ -69,57 +81,77 @@ contract CleanMileDNFT is ERC721, Ownable ,ICleanMileDNFT {
         return _dnftData[_tokenId].dnftType;
     }
 
-    function dnftDescription(uint256 _tokenId) public view returns (string memory) {
+    function dnftDescription(
+        uint256 _tokenId
+    ) public view returns (string memory) {
         return _dnftData[_tokenId].description;
     }
 
-        function dnftName(uint256 _tokenId) public view returns (string memory) {
+    function dnftName(uint256 _tokenId) public view returns (string memory) {
         return _dnftData[_tokenId].name;
     }
 
-    function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+    function tokenURI(
+        uint256 _tokenId
+    ) public view override returns (string memory) {
         return _tokenURIs[_tokenId];
     }
 
-    function upgrade(uint256 _tokenId) external onlyOwner returns(bool){
-        // 토큰이 존재하는지, 토큰이 주인이 맞는지 검사
+    function upgrade(uint256 _tokenId) external returns (bool) {
         if (!_exists(_tokenId)) revert NonexistentToken(_tokenId);
-        if (ownerOf(_tokenId) != msg.sender) revert NotOwner(_tokenId, msg.sender);
-        // 현재 단계를 가져옴
-        uint256 currentLevel = uint256(dnftType(_tokenId))+1;
-        //최대 레벌에서는 업그레이드 불가능
-        if (currentLevel>5) revert MaxLevel(_tokenId);
+        if (ownerOf(_tokenId) != msg.sender)
+            revert NotOwner(_tokenId, msg.sender);
+
+        uint256 currentLevel = uint256(dnftType(_tokenId)) + 1;
+
+        if (currentLevel > 4) revert MaxLevel(_tokenId);
+
         uint256 nextLevel = upgradeCheck(_tokenId);
-        if (nextLevel>currentLevel){
+
+        if (nextLevel > currentLevel) {
             _setTokenURI(_tokenId, IpfsUri[currentLevel]);
             _dnftData[_tokenId].dnftType = DnftType(currentLevel);
             emit UpgradeDNFT(msg.sender, _tokenId);
+            return true;
         }
         return false;
     }
 
-    function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal virtual {
-        if (!_exists(tokenId)) revert NonexistentToken(tokenId); 
+    function _setTokenURI(
+        uint256 tokenId,
+        string memory _tokenURI
+    ) internal virtual {
+        if (!_exists(tokenId)) revert NonexistentToken(tokenId);
         _tokenURIs[tokenId] = _tokenURI;
     }
 
     //외부에 노충된 transfer 함수만 사용 못하게 막아줌.
-    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) onlyOwner public virtual override {
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes memory data
+    ) public virtual override onlyOwner {
         super.safeTransferFrom(from, to, tokenId, data);
     }
+
     //외부에 노충된 transfer 함수만 사용 못하게 막아줌.
-    function transferFrom(address from, address to, uint256 tokenId) onlyOwner public virtual override {
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public virtual override onlyOwner {
         super.transferFrom(from, to, tokenId);
     }
 
-    function setBadge(address badgeAddress) public onlyOwner returns(bool){
-         require(badgeAddress != address(0x0));
-         _badge = ICleanMileBadge(badgeAddress);
-         return true;
+    function setBadge(address badgeAddress) public onlyOwner returns (bool) {
+        require(badgeAddress != address(0x0));
+        _badge = ICleanMileBadge(badgeAddress);
+        return true;
     }
 
-    function upgradeCheck(uint256 _tokenId) public returns(uint256){
-        require(address(_badge) !=  address(0x0), '');
+    function upgradeCheck(uint256 _tokenId) public view returns (uint256) {
+        require(address(_badge) != address(0x0), "");
         address owner = ownerOf(_tokenId);
         uint256 ownerScore = _badge.userBadgeScore(owner);
         // 1단계(씨앗) : 0점(회원가입 상태)~ 14점 (간격: 15점)
@@ -127,10 +159,10 @@ contract CleanMileDNFT is ERC721, Ownable ,ICleanMileDNFT {
         // 3단계(잎새) : 40점 ~ 74점 (간격35점)
         // 4단계(가지) : 75점 ~ 119점 (간격45점)
         // 5단계(열매) : 120점 ~
-        if (ownerScore < 15)  return 1; 
-        if (ownerScore < 40)  return 2; 
-        if (ownerScore < 75)  return 3; 
-        if (ownerScore < 120) return 4; 
+        if (ownerScore < 15) return 1;
+        if (ownerScore < 40) return 2;
+        if (ownerScore < 75) return 3;
+        if (ownerScore < 120) return 4;
         else return 5;
-    } 
+    }
 }
