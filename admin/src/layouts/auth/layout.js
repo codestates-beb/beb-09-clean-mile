e2d1f4f5ddb496fbc4392295d900cb2ff7ddec56
Lee Hyohwak <crewe1746@naver.com>
2023-07-27 19:03:56 +0900

    Remove grids in auth layout

--- admin/src/layouts/auth/layout.js
+++ admin/src/layouts/auth/layout.js
@@ -1,7 +1,7 @@
-import PropTypes from 'prop-types';
-import NextLink from 'next/link';
-import { Box, Typography, Unstable_Grid2 as Grid } from '@mui/material';
-import { Logo } from 'src/components/logo';
+import PropTypes from "prop-types";
+import NextLink from "next/link";
+import { Box } from "@mui/material";
+import { Logo } from "src/components/logo";
 
 // TODO: Change subtitle text
 
@@ -12,100 +12,37 @@ export const Layout = (props) => {
     <Box
       component="main"
       sx={{
-        display: 'flex',
-        flex: '1 1 auto'
+        display: "flex",
+        flex: "1 1 auto",
       }}
     >
-      <Grid
-        container
-        sx={{ flex: '1 1 auto' }}
+      <Box
+        component="header"
+        sx={{
+          left: 0,
+          p: 3,
+          position: "fixed",
+          top: 0,
+          width: "100%",
+        }}
       >
-        <Grid
-          xs={12}
-          lg={6}
+        <Box
+          component={NextLink}
+          href="/"
           sx={{
-            backgroundColor: 'background.paper',
-            display: 'flex',
-            flexDirection: 'column',
-            position: 'relative'
+            display: "inline-flex",
+            height: 32,
+            width: 32,
           }}
         >
-          <Box
-            component="header"
-            sx={{
-              left: 0,
-              p: 3,
-              position: 'fixed',
-              top: 0,
-              width: '100%'
-            }}
-          >
-            <Box
-              component={NextLink}
-              href="/"
-              sx={{
-                display: 'inline-flex',
-                height: 32,
-                width: 32
-              }}
-            >
-              <Logo />
-            </Box>
-          </Box>
-          {children}
-        </Grid>
-        <Grid
-          xs={12}
-          lg={6}
-          sx={{
-            alignItems: 'center',
-            background: 'radial-gradient(50% 50% at 50% 50%, #122647 0%, #090E23 100%)',
-            color: 'white',
-            display: 'flex',
-            justifyContent: 'center',
-            '& img': {
-              maxWidth: '100%'
-            }
-          }}
-        >
-          <Box sx={{ p: 3 }}>
-            <Typography
-              align="center"
-              color="inherit"
-              sx={{
-                fontSize: '24px',
-                lineHeight: '32px',
-                mb: 1
-              }}
-              variant="h1"
-            >
-              Welcome to{' '}
-              <Box
-                component="a"
-                sx={{ color: '#15B79E' }}
-                target="_blank"
-              >
-                Devias Kit
-              </Box>
-            </Typography>
-            <Typography
-              align="center"
-              sx={{ mb: 3 }}
-              variant="subtitle1"
-            >
-              A professional kit that comes with ready-to-use MUI components.
-            </Typography>
-            <img
-              alt=""
-              src="/assets/auth-illustration.svg"
-            />
-          </Box>
-        </Grid>
-      </Grid>
+          <Logo />
+        </Box>
+      </Box>
+      {children}
     </Box>
   );
 };
 
 Layout.prototypes = {
-  children: PropTypes.node
+  children: PropTypes.node,
 };
