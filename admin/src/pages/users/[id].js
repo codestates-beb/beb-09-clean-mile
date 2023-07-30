b99c2d7b8632df911ac6c0fc8437c9cbc1e766ec
Lee Hyohwak <crewe1746@naver.com>
2023-07-30 23:12:01 +0900

    Update user detail page

--- admin/src/pages/users/[id].js
+++ admin/src/pages/users/[id].js
@@ -1,10 +1,20 @@
 import Head from "next/head";
-import { Box, Container, Stack, Typography, Unstable_Grid2 as Grid, Button } from "@mui/material";
+import {
+  Box,
+  Container,
+  Divider,
+  Stack,
+  Typography,
+  Unstable_Grid2 as Grid,
+  Button,
+} from "@mui/material";
 import { Layout as DashboardLayout } from "src/layouts/dashboard/layout";
 import { AccountDnft } from "src/components/account/account-dnft";
 import { AccountWallet } from "src/components/account/account-wallet";
 import { AccountProfileDetails } from "src/components/account/account-profile-details";
 import { useRouter } from "next/router";
+import { PostsTable } from "src/components/posts/posts-table";
+import { CommentsTable } from "src/components/comments/comments-table";
 
 const Page = () => {
   const router = useRouter();
@@ -22,28 +32,30 @@ const Page = () => {
       >
         <Container maxWidth="lg">
           <Stack spacing={3}>
-            <Typography variant="h4">Account</Typography>
-            <Stack direction={"row"} justifyContent="right" spacing={1}>
-              <Button variant="contained" color="warning">
-                Delete
-              </Button>
-              <Button variant="contained" onClick={() => router.back()}>
-                Back
-              </Button>
+            <Stack direction={"row"} justifyContent="space-between" spacing={3}>
+              <Typography variant="h4">Account</Typography>
+              <Stack direction={"row"} spacing={1}>
+                <Button variant="contained" color="warning">
+                  Delete
+                </Button>
+                <Button variant="contained" onClick={() => router.back()}>
+                  Back
+                </Button>
+              </Stack>
             </Stack>
-            <Grid container spacing={2}>
-              <Grid>
-                <AccountProfileDetails />
-              </Grid>
-            </Grid>
-            <Grid container spacing={2}>
-              <Grid xs={12} md={6}>
+            <AccountProfileDetails />
+            <Grid container wrap={"wrap"} gap={3}>
+              <Grid xs={12} sm={6} md={3}>
                 <AccountDnft />
               </Grid>
+
               <Grid xs={12} md={6}>
                 <AccountWallet />
               </Grid>
             </Grid>
+
+            <PostsTable />
+            <CommentsTable />
           </Stack>
         </Container>
       </Box>
