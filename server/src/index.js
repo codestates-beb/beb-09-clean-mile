9b987cf544a17ae03f08ba39470fd252e4f6c0e5
BEB 09 박상현 <psh03225@naver.com>
2023-08-04 21:38:47 +0900

    Update schema

--- server/src/index.js
+++ server/src/index.js
@@ -1,20 +1,33 @@
 const express = require('express');
-const schedule = require('node-schedule');
 const expressApp = require('./loaders/index.js');
 const config = require('./config/index.js');
-const eventScheduler = require('./schedulers/eventScheduler.js');
+const transferBadges = require("./services/contract/dnftController.js").createDNFT; 
 
 async function startServer() {
   const app = express();
   await expressApp(app);
 
-  // 이벤트 스케줄러 실행
-  await eventScheduler(); // 함수로 호출
+  app.post("/transferBadges", createDNFTHandler);
 
-  // 스케줄 설정
-  schedule.scheduleJob('* * * * *', async () => {
-    await eventScheduler(); // eventScheduler 함수를 스케줄에 등록
-  });
+  //createDNFT 경로의 라우터 핸들러 함수
+  async function createDNFTHandler(req, res) {
+    try {
+      const { email, userType} = req.body; // POST 요청의 body로부터 email과 userType을 받아옵니다.
+
+      // Ethereum 스마트 컨트랙트와 상호작용하여 DNFT를 생성하는 createDNFT 함수 호출
+      const result = await transferBadges(email, userType);
+
+      // 요청 결과에 따라 응답을 처리합니다.
+      if (result.success) {
+        res.status(200).json({ success: true, message: "참여자들에게 badge 전송 성공" });
+      } else {
+        res.status(500).json({ success: false, message: "참여자들에게 badge 전송 실패" });
+      }
+    } catch (err) {
+      console.error("Error:", err);
+      res.status(500).json({ success: false, message: "서버 오류 발생" });
+    }
+  }
 
   app
     .listen(config.port, () => {
@@ -31,3 +44,4 @@ async function startServer() {
 }
 
 startServer();
+
