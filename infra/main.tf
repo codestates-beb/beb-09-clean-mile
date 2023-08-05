886f6b714101f380be6251fadf6a616b8259e0aa
Lee Hyohwak <crewe1746@naver.com>
2023-08-05 20:45:50 +0900

    Update main.tf to apply public access to s3 bucket

--- infra/main.tf
+++ infra/main.tf
@@ -39,6 +39,15 @@ resource "aws_s3_bucket" "s3_bucket" {
   tags   = var.common_tags
 }
 
+resource "aws_s3_bucket_public_access_block" "s3_bucket_public_access_block" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 output "s3_bucket_domain_name" {
   value = aws_s3_bucket.s3_bucket.bucket_domain_name
 }
