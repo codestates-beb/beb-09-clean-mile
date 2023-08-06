541a3f915572a4d27a8f06500d2a62c3997ac36a
Lee Hyohwak <crewe1746@naver.com>
2023-08-06 14:15:13 +0900

    Add nat gateway

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -56,11 +56,6 @@ resource "aws_route_table" "public_route_table" {
 resource "aws_route_table" "private_route_table" {
   vpc_id = aws_vpc.main_vpc.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main_internet_gateway.id
-  }
-
   tags = merge(
     var.common_tags,
     {
@@ -90,4 +85,33 @@ resource "aws_internet_gateway" "main_internet_gateway" {
       Name = "Clean Mile Internet Gateway"
     }
   )
+}
+
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "Clean Mile NAT EIP"
+    }
+  )
+}
+
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnets[0].id
+
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "Clean Mile NAT Gateway"
+    }
+  )
+}
+
+resource "aws_route" "private_nat_route" {
+  route_table_id         = aws_route_table.private_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_gateway.id
 }
