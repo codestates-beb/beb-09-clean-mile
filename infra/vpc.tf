ca02c7431bb4a6fe514e71eba4d221784039be30
Lee Hyohwak <crewe1746@naver.com>
2023-08-06 14:22:57 +0900

    Add vpc security group

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -115,3 +115,40 @@ resource "aws_route" "private_nat_route" {
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = aws_nat_gateway.nat_gateway.id
 }
+
+resource "aws_security_group" "main_security_group" {
+  name        = "Clean Mile Security Group"
+  description = "Clean Mile Security Group"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    description = "Allow http inbound traffic"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow https inbound traffic"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "Clean Mile Security Group"
+    }
+  )
+}
