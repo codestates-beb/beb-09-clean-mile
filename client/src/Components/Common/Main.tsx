50332bb715a610fb64f7444de66b9452169a4b4d
KimSeoYeon23 <sollucidus0221@naver.com>
2023-08-08 17:49:46 +0900

    Update favicon

--- client/src/Components/Common/Main.tsx
+++ client/src/Components/Common/Main.tsx
@@ -4,14 +4,14 @@ import useTranslation from 'next-translate/useTranslation';
 import { useRouter } from 'next/router';
 import { hero_img, insta_icon, insta_logo } from '../Reference';
 
-const Main = () =>  {
+const Main = () => {
   const router = useRouter();
   const { t } = useTranslation('common');
 
   const dummy = [
-    { id: 1, image: hero_img, insta_id: 'test1', insta_content: 'skfjklsfjklsjfkljsklf' },
-    { id: 2, image: insta_icon, insta_id: 'test1', insta_content: 'skfjklsfjklsjfkljsklf' },
-    { id: 3, image: insta_logo, insta_id: 'test1', insta_content: 'sfssssssssssssssssssssssssssssssssssssss' },
+    { id: 1, image: hero_img, insta_id: 'et', insta_content: 'Et neque dolores quia veritatis rerum aliquam labore asperiores.Dolorem sequi itaque velit nihil natus fuga.Quis alias voluptas repudiandae iusto inventore neque.Et consequatur dolorum.' },
+    { id: 2, image: insta_icon, insta_id: 'rem', insta_content: 'Ea maxime dolore quam doloremque dolores exercitationem.' },
+    { id: 3, image: insta_logo, insta_id: 'voluptas', insta_content: 'Porro dolorem tenetur animi aperiam ea autem.' },
   ]
 
   return (
@@ -19,13 +19,15 @@ const Main = () =>  {
       <div className='hero-bg py-[10%] md:py-[2%] sm:py-[5%] xs:py-[4%] px-[20%] lg:px-[5%] md:px-[3%] sm:px-[1%] xs:px-[2%]'>
         <div className='w-full h-full flex flex-col justify-center md:justify-end sm:justify-end xs:justify-end md:items-center sm:items-center xs:items-center md:pb-5 sm:pb-2 xs:pb-2 items-end gap-12 lg:gap-6 md:gap-4 sm:gap-4 xs:gap-4 text-right md:text-center sm:text-center xs:text-center'>
           <h1 className='text-7xl font-bold lg:text-4xl md:text-3xl sm:text-2xl xs:text-lg'>Clean Mile</h1>
-          <p className='whitespace-normal w-[50%] lg:w-[70%] md:w-[90%] sm:w-[100%] xs:w-[100%] lg:text-sm md:text-xs sm:text-xs xs:text-xs md:font-semibold sm:font-semibold xs:font-semibold'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Cras rutrum, nulla eu dapibus vestibulum, nisi ligula sodales urna, sodales tincidunt metus odio quis tellus. 
-            Praesent vel facilisis turpis. Maecenas eu lectus risus. 
-            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
-            Sed at tempus est, vitae dictum dui. Aliquam laoreet eget est non venenatis. Ut vestibulum sapien euismod mauris condimentum elementum. 
-            Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+          <p className='whitespace-normal w-[60%] lg:w-[70%] md:w-[90%] sm:w-[100%] xs:w-[100%] lg:text-sm md:text-xs sm:text-xs xs:text-xs font-semibold md:font-semibold sm:font-semibold xs:font-semibold'>
+            <span>
+              {t('common:It is a community service that aims to collect information about environmental protection events in one place to improve accessibility and user-friendliness, and to encourage active participation by issuing certification badges (NFTs) to users who participate in events')}
+            </span>
+            <br />
+            <br />
+            <span className='mt-2'>
+              {t('common:The project was born out of the idea to facilitate interaction between people interested in protecting the environment, and to encourage participants to contribute to solving and improving environmental problems such as urban aesthetics, impaired drainage, loss of biodiversity, air pollution, and resource depletion')}
+            </span>
           </p>
           <div className='flex justify-end items-end w-full md:justify-center sm:justify-center xs:justify-center'>
             <button className='bg-main-blue hover:bg-blue-600 rounded-xl text-white font-semibold px-20 py-3 md:px-16 sm:px-12 xs:px-6 md:text-sm sm:text-sm xs:text-xs transition duration-300'
@@ -64,11 +66,11 @@ const Main = () =>  {
                 cursor-pointer"
                 key={item.id}>
                 <div className='border-b-2 relative pb-[65%] sm:pb-[90%] xs:pb-[90%]'>
-                  <Image 
+                  <Image
                     className='rounded-t-3xl object-cover'
-                    src={item.image} 
+                    src={item.image}
                     layout='fill'
-                    alt='insta image' 
+                    alt='insta image'
                   />
                 </div>
                 <div className='p-6 sm:p-3 xs:p-2 w-full'>
@@ -84,7 +86,7 @@ const Main = () =>  {
       </div>
     </div>
   );
-  
+
 }
 
 export default Main;
